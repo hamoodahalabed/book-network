b4afac53364168bf848260d73edf79f9d77009e4
Mohammad Alabed <mohammad.alabed@inspirejo.com>
2024-09-26 09:29:17 +0300

    security follow up

---
+++ src/main/java/com/mohammad/book_network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.mohammad.book_network.security;
+
+import com.mohammad.book_network.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
