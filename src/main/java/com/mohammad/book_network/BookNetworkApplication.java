d08173f86ab88759575afd414acf7e76751228ee
Mohammad Alabed <mohammad.alabed@inspirejo.com>
2024-10-20 09:30:23 +0300

    some adding, cleaning

--- src/main/java/com/mohammad/book_network/BookNetworkApplication.java
+++ src/main/java/com/mohammad/book_network/BookNetworkApplication.java
@@ -1,11 +1,7 @@
 package com.mohammad.book_network;
 
-import com.mohammad.book_network.role.Role;
-import com.mohammad.book_network.role.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -18,13 +14,5 @@ public class BookNetworkApplication {
 		SpringApplication.run(BookNetworkApplication.class, args);
 
 
-	}
-	@Bean
-	public CommandLineRunner runner(RoleRepository roleRepository) {
-		return args -> {
-			if (roleRepository.findByName("USER").isEmpty()) {
-				roleRepository.save(Role.builder().name("USER").build());
-			}
-		};
 	}
 }
